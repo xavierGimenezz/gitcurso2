639882d4900484c2c4311f9ea77ae1f0a6e084c9
xavierGimenezz <xavier.a.gimenez@gmail.com>
2016-03-30 17:24:50 +0200

    hola
    23

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+	private static String nombre = "david gay";
+	public static void main (String[]args){
+		System.out.println("hola");
+		System.out.println(nombre);
+		
+	}
+}
